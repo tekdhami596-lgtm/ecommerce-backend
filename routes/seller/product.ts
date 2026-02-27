71abceb5f55a5f0cc6539ece62fa82d5e482fa9f
tekdhami596--lgtm <tekdhami596@gmail.com>
2026-02-27 21:09:57 +0545

    ghjk

--- routes/seller/product.ts
+++ routes/seller/product.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import productController from "../../controllers/productController";
-import { upload } from "../../config/cloudinary"; 
+import { upload } from "../../config/cloudinary";
 
 const router = Router();
 
@@ -8,13 +8,18 @@ router.get("/", productController.getSellerProducts);
 router.get("/:id", productController.getSingleProduct);
 router.post(
   "/",
-  upload.array("images[]", 12), 
+  upload.array("images[]", 12),
+  (req, res, next) => {
+    console.log("Uploaded files:", req.files);
+    next();
+  },
+
   productController.createSellerProduct,
 );
 router.delete("/:id", productController.deleteSellerProduct);
 router.patch(
   "/:id",
-  upload.array("images", 12), 
+  upload.array("images", 12),
   productController.updateSellerProduct,
 );
 
